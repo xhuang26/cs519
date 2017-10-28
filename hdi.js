decce3f0272c4bea13b8b4be188d1870538c0115
Siwakorn Srisakaokul <sping128@gmail.com>
2017-10-28 02:39:49 -0500

    Clean up

--- hdi.js
+++ hdi.js
@@ -1,63 +1,80 @@
 (function() {
-    var width = 960;
-    var	height = 1000;
+    var width = 650;
+    var height = 650;
     var num_countries = 188;
-
     var hdi_d3 = d3.select("#hdi");
     var svg = hdi_d3.append("svg");
     svg.attr("width", width);
     svg.attr("height", height);
 
     var projection = d3.geoMercator()
-      .scale([100])
-      .center([0,0])
-      .translate([width/2, height/2]);
+        .scale([100])
+        .translate([width/2, height/2]);
 
     var path = d3.geoPath().projection(projection);
 
+    var x = d3.scaleThreshold()
+        .domain(d3.range(1, num_countries))
+        .range(d3.range(width - 200, width - 20));
+    var x2 = d3.scaleLinear()
+        .domain([1, num_countries])
+        .range([width - 200, width - 20]);
+    // Drawing the scale bar on the map
+    var g = svg.append("g")
+        .attr("class", "key")
+        .attr("transform", "translate(0,30)");
+    g.selectAll("rect")
+      .data(x.domain())
+      .enter().append("rect")
+        .attr("height", 5)
+        .attr("x", function(d) {  return x(d); })
+        .attr("width", function(d) { return x(2) - x(1); })
+        .attr("fill", function(d) { return d3.interpolateRdPu(d / num_countries); });
 
-    var channels = {
-      h: {scale: d3.scaleLinear().domain([0, 360]).range([0, 360]), x: 300},
-      s: {scale: d3.scaleLinear().domain([0, 1]).range([0, 1]), x: 0.5},
-      l: {scale: d3.scaleLinear().domain([0, 1]).range([0, 1]), x: 0.4},
-    };
+    g.append("text")
+    .attr("x", x.range()[0])
+    .attr("y", -10)
+    .attr("class", "caption")
+    .attr("fill", "#000")
+    .attr("text-anchor", "start")
+    .attr("font-weight", "bold")
+    .text("HDI Rank");
 
+    g.call(d3.axisBottom(x2)
+    .tickSize(10)
+    .tickFormat(function(x, i) {
+        return x
+    })
+    .tickValues(x2.domain()))
+        .select(".domain")
+        .remove();
 
     var countryToHDIRank = d3.map();
     d3.queue()
-    	.defer(d3.json, "countries.geojson")
+        .defer(d3.json, "countries.geojson")
         .defer(d3.tsv, "hdi_table1.tsv", function(d) {
             countryToHDIRank.set(d["ISO_A3"], d["HDI rank"]);
         })
-    	.await(ready);
+        .await(ready);
 
 
     function ready(error, countries) {
-    	if (error) throw error;
+        if (error) throw error;
 
-    	svg.selectAll("path")
-    			.data(countries.features)
-    			.enter().append("path")
-    			.attr('d', path)
-    			.attr('vector-effect', 'non-scaling-stroke')
-    			.style("fill", function(d) {
-    				var cur = 10
-                    if (countryToHDIRank.has(d.properties.ISO_A3)) {
-                        // HDI rank for each country
-                        var rank = parseFloat(countryToHDIRank.get(d.properties.ISO_A3))
-                        //console.log(d.properties.ISO_A3 + "," + rank);
-                        var current = d3.hsl(
-                            channels.h.scale.invert(channels.h.x),
-                            channels.s.scale.invert(channels.s.x),
-                            channels.l.scale.invert(channels.l.x)
-                        );
-                        current['s'] = 1 - rank / num_countries;
-                        // console.log(current['h']);
-                        // console.log(current['s']);
-                        // console.log(current['l']);
-                        cur = d3.rgb(current);
-                    }
-    				return cur;
-    			});
+        svg.selectAll("path")
+            .data(countries.features)
+            .enter().append("path")
+            .attr('d', path)
+            .attr('vector-effect', 'non-scaling-stroke')
+            .style("fill", function(d) {
+                var cur = d3.rgb("black");
+                if (countryToHDIRank.has(d.properties.ISO_A3)) {
+                    // HDI rank for each country
+                    var rank = parseFloat(countryToHDIRank.get(d.properties.ISO_A3))
+                    //console.log(d.properties.ISO_A3 + "," + rank);
+                    cur = d3.interpolateRdPu(rank / num_countries)
+                }
+                return cur;
+            });
     }
 })();
--- index.js
+++ index.js
@@ -1,40 +1,38 @@
 (function() {
-    var width = 480;
-	var height = 500;
-
+    var width = 700;
+    var height = 700;
     var random_d3 = d3.select("#random");
     var svg = random_d3.append("svg");
     svg.attr("width", width);
     svg.attr("height", height);
 
+    var projection = d3.geoMercator()
+        .scale([100])
+        .translate([width/2, height/2]);
+
+    var path = d3.geoPath().projection(projection);
+
     var color = d3.scaleThreshold()
         .domain(d3.range(2, 10))
         .range(d3.schemeBlues[9]);
 
-    var projection = d3.geoMercator()
-      .scale([100])
-      .center([0,0])
-      .translate([width/2, height/2]);
-
-    var path = d3.geoPath().projection(projection);
-
     d3.queue()
-    	.defer(d3.json, "countries.geojson")
-    	.await(ready);
+        .defer(d3.json, "countries.geojson")
+        .await(ready);
 
     function ready(error, countries) {
-    	if (error) throw error;
+        if (error) throw error;
 
-    	svg.selectAll("path")
-    			.data(countries.features)
-    			.enter().append("path")
-    			.attr('d', path)
-    			.attr('vector-effect', 'non-scaling-stroke')
-    			.style("fill", function(d) {
-    				// console.log(d);
-    				cur = color(Math.floor(Math.random() * 8) + 2 );
-    				// console.log(cur);
-    				return cur;
-    			});
+        svg.selectAll("path")
+            .data(countries.features)
+            .enter().append("path")
+            .attr('d', path)
+            .attr('vector-effect', 'non-scaling-stroke')
+            .style("fill", function(d) {
+                // console.log(d);
+                cur = color(Math.floor(Math.random() * 8) + 2 );
+                // console.log(cur);
+                return cur;
+            });
     }
 })();
